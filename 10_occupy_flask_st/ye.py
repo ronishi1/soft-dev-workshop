993eefe4037cdf7adb18a5ff5b8a2b794139d54c
Ray <rayonishi11@gmail.com>
2018-09-24 17:21:39 -0400

    Added comments to html and made it easier to read, also updated ye.py

--- 10_occupy_flask_st/ye.py
+++ 10_occupy_flask_st/ye.py
@@ -8,6 +8,16 @@ import csv, random
 
 from flask import Flask, render_template
 app = Flask(__name__)
+#function for returning a random key based on the weights in the dict
+def randomKey(dict):
+    keys = []
+    for key in dict:
+        if key != 'Job Class':#job class and total arent to be chosen from
+            if key != 'Total':
+                keys = keys + [key] * int(dict[key] * 10)
+    return random.choice(keys)
+
+
 
 @app.route("/occupations")
 def occRoute():
@@ -16,27 +26,16 @@ def occRoute():
     with open('data/occupations.csv') as file:
         csvReader = csv.reader(file, delimiter = ',')
         for row in csvReader:
-            if row[0] != "Job Class":
+            if row[0] != "Job Class":#just to make sure we dont accidentally turn a string into a float
                 occDict[row[0]] = float(row[1])
             else:
                 occDict[row[0]] = row[1]
 
 
-    # selects a random occupation
-    occupation = []
-    for key in occDict:
-        if key != 'Job Class':
-            if key != 'Total':
-                occupation = occupation + [key] * int(occDict[key] * 10)
-    ranOcc = random.choice(occupation)
-
-    # testing the ranOcc
-    # print(ranOcc)
-
-    return render_template('occ.html',
+    return render_template('occ.html',#rendering the template with set params
                            title ='Occupation Data',
                            heading = 'Here\'s some cool data on occupation percentages',
-                           occ = ranOcc,
+                           occ = randomKey(occDict),
                            dict = occDict
     )
 
