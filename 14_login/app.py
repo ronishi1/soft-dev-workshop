38f3708e280d1268717be863a5e67569956f97c9
Ray <rayonishi11@gmail.com>
2018-10-01 21:46:55 -0400

    Added comments to code

--- 14_login/app.py
+++ 14_login/app.py
@@ -9,36 +9,51 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+#hardcoded user
 user1 = "tester"
 pass1 = "test"
 
 error = ""
 
+#checks the credentials against the hard coded user
 def check_credentials(username,password):
     return username == user1 and password == pass1
 
 @app.route("/")
 def home():
+    global error
+    #if the user is in session, render the welcome page
     if "user" in session:
         return render_template('auth.html',username=session["user"])
-    return render_template('login.html',errorMessage=error)
+    #resetting error after the message has been stored
+    errorMessage = error
+    error = ""
+    #otherwise render the login page, shows an error if bad username/password was inputted
+    return render_template('login.html',errorMessage=errorMessage)
 
 @app.route("/logout")
 def logout():
+    #removes the user from the session
     session.pop("user")
+    #redirects to the home page after logging user out
     return redirect(url_for('home'))
 
 @app.route("/auth",methods=["POST"])
 def authenticate():
+    #needed to modify the global variable error (to be used in home)
     global error
     username = request.form['username']
     password = request.form['password']
+    #if the credentials match, add the user to session
     if check_credentials(username,password):
         session["user"] = username
+    #checks for bad username
     elif username != user1:
         error = "bad username!"
+    #otherwise it has to be a bad password
     else:
         error = "bad password!"
+    #redirecting to home
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
