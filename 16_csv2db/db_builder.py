06c76443bacc26fac84efc9813a1442265f818cf
Ray <rayonishi11@gmail.com>
2018-10-04 20:41:19 -0400

    Added comments

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -6,9 +6,14 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
+# Creates a cursor and the database
 db = sqlite3.connect("discobandit.db")
 c = db.cursor()
 
+# Dictionaries of all the headers along with their type
+# We pass this into the function as a parameter
+# We hardcoded the headers because we weren't sure
+# how to type the different columns
 peeps = {"name":"TEXT","age":"INTEGER","id":"INTEGER"}
 occupations = {"Job Class":"TEXT","Percentage":"INTEGER"}
 courses = {"code":"TEXT","mark":"INTEGER","id":"INTEGER"}
@@ -16,23 +21,30 @@ courses = {"code":"TEXT","mark":"INTEGER","id":"INTEGER"}
 def buildTable(file,headers,tableName):
     headerInfo = ""
     for key in headers:
+        # turns the headers from a dictionary into a string
         headerInfo += key + " " + headers[key] + ","
     headerInfo = headerInfo[:-1]
     print(headerInfo)
     c.execute("CREATE TABLE " + tableName + "(" + headerInfo + ")")
     with open(file) as csvfile:
+        # read the CSV File
         reader = csv.DictReader(csvfile)
         for row in reader:
+            # build the command we will execute
             cmd = "("
             for key in headers:
+                # The values that are of type TEXT are enclosed by quotes
                 if headers[key] == "TEXT":
                     cmd += "\"" + row[key] + "\","
+                # if they are of any other type (namely INTEGER in this case)
+                # they are added as is
                 else:
                     cmd += row[key] + ","
             cmd = cmd[:-1]
             cmd += ")"
+            # execute the command
             c.execute("INSERT INTO " + tableName + " VALUES" + cmd)
-
+#run this function on the three CSV files
 buildTable('peeps.csv',peeps,"peeps")
 buildTable('occupations.csv',occupations,"occupations")
 buildTable('courses.csv',courses,"courses")
